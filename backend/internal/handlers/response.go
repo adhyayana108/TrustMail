e8273db36cad4c7b26203eb59e3617a40dd79a35
adhyayana108 <adhyayana108@gmail.com>
2026-07-31 05:51:17 +0530

    response

--- backend/internal/handlers/response.go
+++ backend/internal/handlers/response.go
@@ -6,7 +6,7 @@ import (
 )
 
 func writeJSON(w http.ResponseWriter, status int, v interface{}) {
-	w.Header().Set("Content-Type", application/json)
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	json.NewEncoder(w).Encode(v)
 }
